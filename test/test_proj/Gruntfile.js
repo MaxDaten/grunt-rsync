81f39bfa8bc06df9619dc5273f27c1ea47eddd1e
Jan-Philip Loos <maxdaten@gmail.com>
2013-08-17 23:39:14 +0200

    test proj

---
+++ test/test_proj/Gruntfile.js
@@ -0,0 +1,24 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+
+  // Project configuration.
+  grunt.initConfig({
+    rsync: {
+      template: {
+        files: '../test-files/',
+        options: {
+          host: '192.168.178.200',
+          user: 'jloos',
+          remoteBase: '/home/jloos/grunt-test-dir/'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-rsync-2');
+  
+  // Default task.
+  grunt.registerTask('default', 'rsync');
+
+};
